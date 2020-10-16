e1349215e5fc365c09672efff84244f277e553a3
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-10-16 11:32:09 +0800

    Updated conv.fs .

--- conv.fs
+++ conv.fs
@@ -4,22 +4,28 @@
     ADC! ADC@
 ;
 
-variable adcCh
+\ conv samples adcInp 64 times and averages the samples
+\ leaving the result n on the data stack.
 : conv ( adcInp -- n )
-    adcCh !
-    adcCh @ adc.sample
+    dup ( c -- c c )
+    adc.sample ( c c -- c n )
 
     \ sample 63 more times and sum
     62 for
-        adcCh @ adc.sample
-        +
+        swap ( c n -- n c )
+        dup ( n c -- n c c )
+        adc.sample ( n c c -- n c ns )
+        rot ( n c ns -- c ns n )
+        + ( c ns n -- c ns )
     next
 
-    \ divide by 64
+    \ divide by 64 ( c ns -- c nd )
     5 for
         2/
     next
 
-    \ make it unsigned
+    \ make it unsigned ( c nd -- c nu )
     $3ff and
+
+    swap drop ( c nu -- nu )
 ;
