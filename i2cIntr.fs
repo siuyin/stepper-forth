6d8adf7be0428ddc407a3043dac2228b1afabce1
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-10-31 20:39:20 +0800

    Updated i2c interrupt routine.

--- i2cIntr.fs
+++ i2cIntr.fs
@@ -5,19 +5,23 @@ RAM
 \res MCU: STM8S103
 #require :NVM
 #require ]B?
+#require ]B!
 
 \res export INT_I2C
 \res export I2C_SR1 I2C_SR3
+\res export I2C_CR2
 
 :NVM
     savec
     [ I2C_SR1 1 ]B? \ address matched interrupt?
     if
+        dbH
         i2cClrAddr
     then
 
     i2cStopped? if
-        dbH \ turn on LED
+        dbL
+        [ 1 I2C_CR2 1 ]B! \ set STOP to release SDA and SCL
     then
     iret
 ;NVM INT_I2C !
