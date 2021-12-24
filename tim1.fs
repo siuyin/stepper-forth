af0f4369fbf70984589dd0721ff2c419b3fdfd51
Loh Siu Yin <siuyin@beyondbroadcast.com>
2021-12-24 04:33:03 +0000

    Refactored tim1.fs to have exports close to usage.

--- tim1.fs
+++ tim1.fs
@@ -1,20 +1,21 @@
-\res MCU: STM8S103
-\res export TIM1_PSCRH TIM1_ARRH TIM1_CR1 TIM1_CCMR3 TIM1_CCER2 TIM1_BKR TIM1_CCR3H TIM1_CNTRH
-\res export TIM1_CCMR4 TIM1_CCR4H
-#require ]B!
-
 NVM
+
+\res MCU: STM8S103
 
 \ The counter clock frequency is calculated as follows:
 \ f CK_CNT = f CK_PSC /(PSCR[15:0]+1)
+\res export TIM1_PSCRH   \ TIM1 prescaler register high
 : TIM1.prescale ( n -- )
     TIM1_PSCRH 2C!
     ;
 
+\res export TIM1_ARRH    \ TIM1 auto-reload register high
 : TIM1.upperLimit ( n -- ) 
     TIM1_ARRH 2C!
     ;
 
+#require ]B!
+\res export TIM1_CR1     \ TIM1 control register 1
 : TIM1.enableClock ( -- ) 
     [ 1 TIM1_CR1 0 ]B!
     ;
@@ -25,12 +26,17 @@ NVM
     TIM1.enableClock
     ;
 
+\res export TIM1_CCMR3   \ TIM1 capture/compare mode register 3
+\res export TIM1_CCER2   \ TIM1 capture/compare enable register 2
+\res export TIM1_BKR     \ TIM1 break register
 : TIM1.pwm3.init ( -- )
     $68 TIM1_CCMR3 C! \ set timer 1 channel 3 to PWM mode 1
     [ 1 TIM1_CCER2 0 ]B! \ enable channel 3 output
     [ 1 TIM1_BKR 6 ]B! \ AOE: Automatic Output Enable set
     ;
     
+\res export TIM1_CNTRH   \ TIM1 counter high
+\res export TIM1_CCR3H   \ TIM1 capture/compare register 3 high
 : TIM1.pwm3 ( dutyCycle -- )
     0 TIM1_CNTRH 2C! \ reset counter to zero
     TIM1_CCR3H 2C! \ set pwm duty cycle
@@ -42,12 +48,17 @@ NVM
     1999 TIM1.pwm3
     ;
 
+\res export TIM1_CCMR4   \ TIM1 capture/compare mode register 4
+\res export TIM1_CCER2   \ TIM1 capture/compare enable register 2
+\res export TIM1_BKR     \ TIM1 break register
 : TIM1.pwm4.init ( -- )
     $68 TIM1_CCMR4 C! \ set timer 1 channel 4 to PWM mode 1
     [ 1 TIM1_CCER2 4 ]B! \ enable channel 4 output
     [ 1 TIM1_BKR 6 ]B! \ AOE: Automatic Output Enable set
     ;
     
+\res export TIM1_CNTRH   \ TIM1 counter high
+\res export TIM1_CCR4H   \ TIM1 capture/compare register 4 high
 : TIM1.pwm4 ( dutyCycle -- )
     0 TIM1_CNTRH 2C! \ reset counter to zero
     TIM1_CCR4H 2C! \ set pwm duty cycle
