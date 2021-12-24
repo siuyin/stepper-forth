918b5aa7649d87b024e10f6d2245af84db725834
Loh Siu Yin <siuyin@beyondbroadcast.com>
2021-12-24 05:51:42 +0000

    Removed outdated lib and replace with symlink to new folder and updated beep-config.fs .

--- beep-config.fs
+++ beep-config.fs
@@ -1,9 +1,8 @@
-\ beep.fs sets option byte 2 to enable beeper
+\ beep-config.fs sets option byte 2 to enable beeper
 \res MCU: STM8S103
-\res export OPT2
 
+\res export OPT2    \ Options 2 Alternate function remapping
 #require OPT!
-
 : setBeepFunction ( -- )
     $80 OPT2 OPT!
 ;
