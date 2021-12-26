1439489142f21f5234340ad05c7b5717b0503841
Loh Siu Yin <siuyin@beyondbroadcast.com>
2021-12-26 09:43:40 +0000

    Updatd beep-config.fs .

--- beep-config.fs
+++ beep-config.fs
@@ -2,7 +2,9 @@
 \res MCU: STM8S103
 
 \res export OPT2    \ Options 2 Alternate function remapping
+NVM
 #require OPT!
-: setBeepFunction ( -- ) \ This apparently cannot be set in NVM. Why?
+: setBeepFunction ( -- ) \ edit lib/TARGET to NVM to move this to NVM
     $80 OPT2 OPT!
 ;
+RAM
